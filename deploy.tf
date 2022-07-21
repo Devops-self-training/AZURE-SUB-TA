97ffc3f825f570b1b2bec60e4644148d0ee45a36
Macbook1 <macbook1@Macbook1s-MacBook-Pro.local>
2022-07-21 08:36:31 +0700

    update keys

--- deploy.tf
+++ deploy.tf
@@ -1,8 +1,8 @@
 #  Replace these value to your own value
 locals {
-  tenant_id         = "c14b46fc-2780-4bee-bcfa-e3f5a1c337b9"
-  subscription_name = "AZURE-SUB-TA"
-  subscription_id   = "d0767e87-e2a6-4477-a762-768ce1367057"
+  tenant_id         = ""
+  subscription_name = ""
+  subscription_id   = ""
   location          = "southeastasia"
 }
 
